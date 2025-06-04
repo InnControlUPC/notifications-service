0ef015312b90ed1baca56e5afa825b5e24a48ef2
Ryzeon <alex.ramon2004@gmail.com>
2025-06-04 15:45:27 -0500

    feat: gradle to maven notifications-service

--- src/test/java/com/github/inncontrol/notifications/NotificationsServiceApplicationTests.java
+++ src/test/java/com/github/inncontrol/notifications/NotificationsServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.github.inncontrol.notificationsservice;
+package com.github.inncontrol.notifications;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
