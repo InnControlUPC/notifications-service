544f454646bdcbb375e0a262ae713355e067e083
Ryzeon <alex.ramon2004@gmail.com>
2025-06-04 15:50:36 -0500

    feat: gradle to maven

--- src/main/java/com/github/inncontrol/notifications/NotificationsServiceApplication.java
+++ src/main/java/com/github/inncontrol/notifications/NotificationsServiceApplication.java
@@ -1,4 +1,4 @@
-package com.github.inncontrol.notificationsservice;
+package com.github.inncontrol.notifications;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
